988ef4a2b3ff960c036cbac21b6490d7c785de27
Peter Sjöqvist <info@mageparts.com>
2020-12-08 09:44:31 +0100

    M2SF-2
    * Corrected documentation.
    * Removed unnecessary code.
    * Assigned access modifiers to constants.

--- ViewModel/FetchAddress.php
+++ ViewModel/FetchAddress.php
@@ -1,18 +1,7 @@
 <?php
 /**
- * Copyright 2016 Resurs Bank AB
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
  */
 
 declare(strict_types=1);
@@ -25,15 +14,15 @@ use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Resursbank\Simplified\Helper\Api;
 
 /**
- * View model for Resurs Bank's widget to fetch a customer's address based on a
- * given SSN or organisation number.
+ * View model for Resurs Bank's widget to fetch a customer's address based on
+ * provided SSN or organisation number.
  */
 class FetchAddress implements ArgumentInterface
 {
     /**
      * @var FormKey
      */
-    private FormKey $formKey;
+    private $formKey;
 
     /**
      * @param FormKey $formKey
