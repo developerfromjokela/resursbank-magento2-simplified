d2eba4b0e408766d0a43d46908f80beaaac19a10
Peter Sjöqvist <info@mageparts.com>
2021-12-27 09:40:37 +0100

    M2SF-125
    * Removed default values from model getters.

--- Controller/Checkout/Session.php
+++ Controller/Checkout/Session.php
@@ -137,7 +137,7 @@ class Session implements HttpPostActionInterface
         $method = $this->paymentMethodRepo->getByCode($methodCode);
 
         return (
-            $method->getActive(false) &&
+            $method->getActive() &&
             in_array(
                 ($isCompany ? 'LEGAL' : 'NATURAL'),
                 $this->paymentMethods->getCustomerTypes($method),
