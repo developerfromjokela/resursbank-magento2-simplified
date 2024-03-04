c899bba8417eb477f667ed16a0f0c61242110d89
Mikael Jacobson <mikael.jacobson@resurs.se>
2024-03-04 09:50:25 +0100

    MRP-120 Fix docblocks

--- Controller/Checkout/FetchAddress.php
+++ Controller/Checkout/FetchAddress.php
@@ -56,6 +56,8 @@ class FetchAddress implements HttpPostActionInterface
     }
 
     /**
+     * Main execution entry point.
+     *
      * @throws Exception
      * @return ResultInterface
      */
@@ -85,6 +87,8 @@ class FetchAddress implements HttpPostActionInterface
     }
 
     /**
+     * Get address.
+     *
      * @return array<string, mixed>
      * @throws ApiDataException
      * @throws ValidatorException
