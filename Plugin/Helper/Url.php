22234fe9db836f45299f23adba945e4caee084ad
Peter Sjöqvist <peter.sjoqvist@resurs.se>
2024-05-07 07:20:48 +0200

    M2SF-170

--- Plugin/Helper/Url.php
+++ Plugin/Helper/Url.php
@@ -8,7 +8,10 @@ declare(strict_types=1);
 
 namespace Resursbank\Simplified\Plugin\Helper;
 
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Store\Model\StoreManagerInterface;
 use Resursbank\Core\Helper\Url as Subject;
+use Resursbank\Simplified\Helper\Config;
 
 /**
  * Appends '/#payment' to the URL we redirect clients to after the cart has been
@@ -18,18 +21,34 @@ use Resursbank\Core\Helper\Url as Subject;
 class Url
 {
     /**
+     * @param StoreManagerInterface $storeManager
+     * @param Config $config
+     */
+    public function __construct(
+        private readonly StoreManagerInterface $storeManager,
+        private readonly Config $config
+    ) {
+    }
+
+    /**
      * Intercept calls to the getCheckoutRebuildRedirectUrl method.
      *
      * @param Subject $subject
      * @param string $result
      * @return string
+     * @throws NoSuchEntityException
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     * @noinspection PhpUnusedParameterInspection
      */
     public function afterGetCheckoutRebuildRedirectUrl(
         Subject $subject,
         string $result
     ): string {
+        if (!$this->config->isActive(
+            scopeCode: $this->storeManager->getStore()->getCode()
+        )) {
+            return $result;
+        }
+
         return $result . '/#payment';
     }
 }
